0c6c57d86cdaf7d8e8d6d4feb515995f006e1a65
yangxy81118 <yangxy81118@gmail.com>
2016-07-30 10:22:25 +0800

    local inti

---
+++ loghunter-local/src/main/java/com/github/yangxy81118/loghunter/local/console/CommandController.java
@@ -0,0 +1,13 @@
+package com.github.yangxy81118.loghunter.local.console;
+
+/**
+ * 通过命令行来控制当前配置
+ * @author yangxy8
+ *
+ */
+public class CommandController {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ loghunter-local/src/main/java/com/github/yangxy81118/loghunter/local/monitor/LocalConfigMonitor.java
@@ -0,0 +1,16 @@
+package com.github.yangxy81118.loghunter.local.monitor;
+
+
+/**
+ * 本地模式，目前采用指定地点的配置文件监控
+ * @author yangxy8
+ *
+ */
+public class LocalConfigMonitor implements Runnable {
+
+	@Override
+	public void run() {
+		
+	}
+
+}
